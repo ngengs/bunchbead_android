73986406c960a2c83632d909e46f003696d9f259
Rizky Kharisma <r.kharisma29@gmail.com>
2018-07-14 21:53:58 +0700

    Notification

--- app/src/main/java/com/ice/bunchbead/android/helpers/NotificationHelper.java
+++ app/src/main/java/com/ice/bunchbead/android/helpers/NotificationHelper.java
@@ -2,15 +2,19 @@ package com.ice.bunchbead.android.helpers;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import timber.log.Timber;
+
 /**
  * Created by rizky Kharisma on 14/07/18.
  */
 public class NotificationHelper {
     public static void registerNotification() {
-        FirebaseMessaging.getInstance().subscribeToTopic("rank-notification");
+        FirebaseMessaging.getInstance().subscribeToTopic("rank")
+                .addOnCompleteListener(task -> Timber.d("Registered rank-notification"));
     }
 
     public static void unregisterNotification() {
-        FirebaseMessaging.getInstance().unsubscribeFromTopic("rank-notification");
+        FirebaseMessaging.getInstance().unsubscribeFromTopic("rank")
+                .addOnCompleteListener(task -> Timber.d("Unregistered rank-notification"));
     }
 }
--- app/src/main/java/com/ice/bunchbead/android/notifications/MessagingService.java
+++ app/src/main/java/com/ice/bunchbead/android/notifications/MessagingService.java
@@ -1,11 +1,11 @@
 package com.ice.bunchbead.android.notifications;
 
-import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.TaskStackBuilder;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -33,7 +33,7 @@ public class MessagingService extends FirebaseMessagingService {
             if (remoteMessage.getNotification().getTitle() != null) {
                 title = remoteMessage.getNotification().getTitle();
             }
-        } else if (remoteMessage.getData() != null) {
+        } else if (remoteMessage.getData().size() > 0) {
             if (remoteMessage.getData().containsKey("title")) {
                 title = remoteMessage.getData().get("title");
             }
@@ -43,17 +43,19 @@ public class MessagingService extends FirebaseMessagingService {
         }
 
         Intent intent = new Intent(this, NotificationResultActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(intent);
 
-        Notification notificationCompat = new NotificationCompat.Builder(this, "")
-                .setContentIntent(PendingIntent.getActivity(this, 0, intent, 0))
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "bunchbead_notification")
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setContentIntent(stackBuilder.getPendingIntent(0, PendingIntent.FLAG_CANCEL_CURRENT))
                 .setContentTitle(title)
                 .setContentText(body)
                 .setColor(UtilHelper.getColor(this, R.color.colorPrimary))
                 .setAutoCancel(true)
                 .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .build();
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify("ingredient", 100, notificationCompat);
+        notificationManager.notify(100, notificationBuilder.build());
     }
 }
